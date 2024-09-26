66fd1c5b807421e77e2fb0b59aa9d206f628d914
Josh <josh@git.com>
2024-09-26 06:20:53 +0200

    feat(database): Implement Database Connection Using pgx

---
+++ internal/infra/db/db.go
@@ -0,0 +1,55 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"iohk-golang-backend-preprod/internal/config"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewDBPool(ctx context.Context, cfg *config.Config) (*pgxpool.Pool, error) {
+	// TODO: put sslmode in env variables
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		cfg.PostgresUser,
+		cfg.PostgresPassword,
+		cfg.PostgresHost,
+		cfg.PostgresPort,
+		cfg.PostgresDB,
+	)
+
+	poolConfig, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse database connection string: %w", err)
+	}
+
+	// TODO: put this in env variables
+	poolConfig.MaxConns = 25
+	poolConfig.MinConns = 5
+	poolConfig.MaxConnLifetime = 5 * time.Hour
+	poolConfig.MaxConnIdleTime = 15 * time.Minute
+	poolConfig.HealthCheckPeriod = 1 * time.Minute
+
+	pool, err := pgxpool.NewWithConfig(ctx, poolConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create connection pool: %w", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		pool.Close()
+		return nil, fmt.Errorf("unable to ping database: %w", err)
+	}
+
+	log.Println("Successfully connected to the database")
+	return pool, nil
+}
+
+func CloseDBPool(pool *pgxpool.Pool) {
+	if pool != nil {
+		pool.Close()
+		log.Println("Database connection pool closed")
+	}
+}
